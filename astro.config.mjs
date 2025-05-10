24f091bb778f5c65701511d3648c4709611b11fc
SharpArk <sharkingbenitezjeisson@gmail.com>
2025-05-10 14:24:07 -0500

    chore: update tsconfig.json to include JSX support for React

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,21 @@ import { defineConfig, envField } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "server",
+
   env: {
     schema: {
-      API_URL: envField.string({ context: "server", access: "secret" }),
+      API_URL: envField.string({ context: "server", access: "public" }),
     },
   },
+
+  integrations: [react()],
 });
